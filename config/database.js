813a10f6b91f58ca8471c568f6eef2483c6758ef
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-21 15:25:15 -0600

    cambios en variables de entorno

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-ccq7ssaen0hr84g7654g-a.oregon-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'guitarla_rwpy'),
-      user: env('DATABASE_USERNAME', 'tikodev'),
-      password: env('DATABASE_PASSWORD', 'sBS4K2nutW5zwXHBfmhRydW7a5XrOcYC'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
